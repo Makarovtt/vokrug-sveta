49cc35e347fe6c389d2d3d928bd96b962b596301
Makarovtt <makarovt_t@mail.ru>
2024-11-14 16:11:08 +0400

    again3

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import * as React from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+// import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
 
-import { cn } from "@/lib/utils";
-import { buttonVariants } from "./Button";
+// import { cn } from "@/lib/utils";
+// import { buttonVariants } from "./Button";
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
